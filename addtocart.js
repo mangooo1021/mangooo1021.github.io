4c75e043a79595a2b4edeef93d600994de7ac924
mangooo1021 <smango62@gmail.com>
2023-01-11 02:44:31 +0800

    綁js

---
+++ addtocart.js
@@ -0,0 +1,93 @@
+const product = [
+  {
+    id: 0,
+    image: "image/Group-16.jpg",
+    title: "水源絕對環保塑膠袋",
+    price: 120,
+  },
+  {
+    id: 1,
+    image: "image/Group-17.jpg",
+    title: "水源紙袋",
+    price: 60,
+  },
+  {
+    id: 2,
+    image: "image/Group-20.jpg",
+    title: "水源圍裙",
+    price: 0,
+  },
+  {
+    id: 3,
+    image: "image/Group-19.jpg",
+    title: "大帆布袋",
+    price: 100,
+  },
+];
+const categories = [
+  ...new Set(
+    product.map((item) => {
+      return item;
+    })
+  ),
+];
+let i = 0;
+document.getElementById("root").innerHTML = categories
+  .map((item) => {
+    var { image, title, price } = item;
+    return (
+      `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image}></img>
+            </div>
+        <div class='bottom'>
+        <p>${title}</p>
+        <h2>$ ${price}.00</h2>` +
+      "<button onclick='addtocart(" +
+      i++ +
+      ")'>加入購物車</button>" +
+      `</div>
+        </div>`
+    );
+  })
+  .join("");
+
+var cart = [];
+
+function addtocart(a) {
+  cart.push({ ...categories[a] });
+  displaycart();
+}
+function delElement(a) {
+  cart.splice(a, 1);
+  displaycart();
+}
+
+function displaycart() {
+  let j = 0,
+    total = 0;
+  document.getElementById("count").innerHTML = cart.length;
+  if (cart.length == 0) {
+    document.getElementById("cartItem").innerHTML = "購物車目前是空的喔";
+    document.getElementById("total").innerHTML = "$ " + 0 + ".00";
+  } else {
+    document.getElementById("cartItem").innerHTML = cart
+      .map((items) => {
+        var { image, title, price } = items;
+        total = total + price;
+        document.getElementById("total").innerHTML = "$ " + total + ".00";
+        return (
+          `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>$ ${price}.00</h2>` +
+          "<i class='fa-solid fa-trash' onclick='delElement(" +
+          j++ +
+          ")'></i></div>"
+        );
+      })
+      .join("");
+  }
+}
